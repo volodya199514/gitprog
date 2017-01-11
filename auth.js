5b106437f750f1d1fce899585b91b714b51d39f3
volodya199514 <volodya199514@gmail.com>
2017-01-11 13:18:45 +0200

    implement auth

---
+++ auth.js
@@ -0,0 +1,9 @@
+/**
+ * Created by User on 11.01.17.
+ */
+function auth(login, pass){
+    if(login === 'login' && pass === 'password')
+        return "you were logged in";
+    else
+        return "login is incorrect";
+}
